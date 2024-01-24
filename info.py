a0c94a70e1e9289e72fb7f022cb6fe3d767902fe
md8756 <157083099+md8756@users.noreply.github.com>
2024-01-24 12:31:51 +0800

    Update info.py

--- info.py
+++ info.py
@@ -4,5 +4,5 @@ BOT_TOKEN    = "6415272673:AAEhs-fgQr-zDTWiBi8EAxXfNSo5tM4Lr4M"
 SESSION      = "BQGdbYEAZq5HCFUQOhydgrA6KYo3DHD11_aZHuDoUKtUm2dOSIKDjBBLuPhh14-nlgU_BqxggiVkOT9dFls5uc8k8_bQn05vIlyE17aUVLgHeHjb2cx-HEvG8DwsdqZcjJ1bf_ErrNpf94SAmoxp0EN0oeaR2CTYn6pYqxE8yvyi5kIi_mKPgnZUeJQO05YG0ZgqIay2oJRK2q8n6IzEns5Rgq7r68FMhT9IfYtv0PXgEUzatQjrRPXf5YrO_MaAg_tzW5oV3FQJQbGFfnwevM4o5owups_D0swlvlDgshqc-gq6uthyeZwBRn29yet4WfOxlY9L5IXpDNMiPKkNhnUoc_svxgAAAAFq8Tr4AA"
 DATABASE_URI = "mongodb+srv://movie700:movie700@cluster0.rklxod8.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  =  -1001901731410
-ADMIN        = 6747402025
-CHANNEL      = "Movie_service"
+ADMIN        =  5845115673
+CHANNEL      = "@netflixhindimovie24"
